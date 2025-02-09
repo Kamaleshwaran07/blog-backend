382f3fde30eae5acbea5d6bb9e4cb2177dacd595
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2025-02-10 00:14:00 +0530

    env added

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ import postRoutes from './routes/posts.js'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import multer from 'multer'
+import dotenv from 'dotenv'
 const app = express()
 
+dotenv.config()
 app.use(express.json())
 
-app.use(cors({origin:"http://localhost:5173", credentials:true}))
+app.use(cors({origin:process.env.baseurl, credentials:true}))
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
