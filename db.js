8807babdab8cfa37e755eed25191d07fb56861d4
Kamaleshwaran07 <pskamaleshwaran@gmail.com>
2025-02-10 00:17:14 +0530

    db password removed

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 import mysql from 'mysql2';
-
+import dotenv from "dotenv"
+dotenv.config()
 export const db = mysql.createConnection({
     host: 'localhost',
     user:"root",
-    password: "mysql",
+    password: process.env.dbPassword,
     database: "blog",
     
 });
